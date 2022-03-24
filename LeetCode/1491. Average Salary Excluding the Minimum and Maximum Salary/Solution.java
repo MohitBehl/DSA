a6848962b00c6dd5344ed9feb2caf728aaa8c62c
Mohit Behl <mohitbehlofficial@gmail.com>
2022-03-24 12:42:22 +0530

    codes

---
+++ LeetCode/1491. Average Salary Excluding the Minimum and Maximum Salary/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public double average(int[] salary) {
+        int sum = 0 , min = Integer.MAX_VALUE , max = Integer.MIN_VALUE;
+        for(int vl : salary){
+            max = Math.max(max,vl);
+            min = Math.min(min,vl);
+            sum += vl;
+        }
+        
+        sum -= max;
+        sum -= min;
+        
+        double ans = (double)sum / (salary.length-2);
+        return ans;
+    }
+}
