f3cfd56a829ca800ef6920b18f29572de43e204a
Mohit Behl <mohitbehlofficial@gmail.com>
2022-04-04 16:33:09 +0530

    Create Solution.java

---
+++ Company Wise/1. Amazon/1710 Max units on a truck/Solution.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-units-on-a-truck/
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes,(a,b) -> {return b[1]-a[1];});
+        int idx = 0;
+        int res = 0;
+        
+        while(truckSize > 0 && idx < boxTypes.length){
+            int nboxes = boxTypes[idx][0];
+            int units = boxTypes[idx][1];
+            
+            int qty = Math.min(truckSize,nboxes);
+            truckSize -= qty;
+            res += qty * units;
+            idx++;
+        }
+        return res;
+    }
+}
