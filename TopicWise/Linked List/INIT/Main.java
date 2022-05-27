23b3e7cbf58894f4ffb5c2cba8e347fffb3af6d9
MohitBehl <mohitbehlofficial@gmail.com>
2022-05-27 20:42:17 +0530

    // Floyd Cycle : 141. Linked List Cycle , LeetCode

--- TopicWise/Linked List/INIT/Main.java
+++ TopicWise/Linked List/INIT/Main.java
@@ -12,8 +12,28 @@ public class Main{
         }
         return false;
     }
-    
-    public static void main(String[] args) {
-        
+    // Floyd Cycle : 141. Linked List Cycle , LeetCode
+        /**
+        * Definition for singly-linked list.
+        * class ListNode {
+        *     int val;
+        *     ListNode next;
+        *     ListNode(int x) {
+        *         val = x;
+        *         next = null;
+        *     }
+        * }
+        */
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
     }
+    
 }
