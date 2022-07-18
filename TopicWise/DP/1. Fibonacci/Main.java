38d6c02cf633510d1c16f71cb5eb9e19b4007700
MohitBehl <mohitbehlofficial@gmail.com>
2022-07-18 19:54:02 +0530

    DP : Fibonacci corrected + Climb Stairs
    Climb Stairs :
    Given : a number 'n'  representing number of stairs to climb
    To find : number of ways to climb if at-max 3 length step is allowed from any position.
    Output : a number representing number of ways.

--- TopicWise/DP/1. Fibonacci/Main.java
+++ TopicWise/DP/1. Fibonacci/Main.java
@@ -31,6 +31,9 @@ public class Main{
         return dp[n];
     }
     public static int fib_tabOpti(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
         int first = 0 , second = 1;
         for(int i = 2 ; i <= n ; i++){
             int third = first + second;
