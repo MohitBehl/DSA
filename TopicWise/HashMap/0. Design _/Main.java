fb9c0dee1e1c68e68d65544b3fbc7d42ed849e20
MohitBehl <mohitbehlofficial@gmail.com>
2022-11-25 22:43:56 +0530

    Heap

--- TopicWise/HashMap/0. Design _/Main.java
+++ TopicWise/HashMap/0. Design _/Main.java
@@ -123,23 +123,111 @@ class CustomHashMap{
 }
 
 
+class Heap{
+    ArrayList<Integer> data;
+
+    Heap(){
+        data = new ArrayList<>();
+    }
+
+    private void upheapify(int idx){ // recursive
+        if(idx == 0) return;
+        int pidx = (idx-1) / 2;
+
+        if(data.get(idx) < data.get(pidx)){
+            // current ele has higher priority than par
+            // swap
+            swap(idx,pidx);
+            upheapify(pidx);
+        }
+    }
+
+    private void downheapify(int idx){ // recursive
+        int lidx = (2 * idx) + 1;
+        int ridx = (2 * idx) + 2;
+
+        int min = idx;
+        if(lidx < data.size() && data.get(lidx) < data.get(min)){ // lval exists
+            min = lidx;
+        }
+        if(ridx < data.size() && data.get(ridx) < data.get(min)){ // rval exists
+            min = ridx;
+        }
+
+        if(min != idx){
+            swap(idx,min);
+            downheapify(min);
+        }
+    }
+
+    private void swap(int idx1,int idx2){
+        int v1 = data.get(idx1);
+        int v2 = data.get(idx2);
+
+        data.set(idx1, v2);
+        data.set(idx2, v1);
+    }
+
+    public void add(int vl){
+        data.add(vl);
+        upheapify(data.size()-1);
+    }
+
+    public int remove(){
+        if(data.size() == 0){
+            return -1;
+        }
+        int vl = data.get(0);
+        swap(0,data.size()-1);
+        data.remove(data.size()-1);
+        downheapify(0);
+
+        return vl;
+    }
+
+    public int min(){
+
+        if(data.size() == 0){
+            return -1;
+        }
+
+        return data.get(0);
+    }
+
+}
+
 public class Main{
     public static void main(String[] args) {
-        CustomHashMap hm = new CustomHashMap();
+        // CustomHashMap hm = new CustomHashMap();
     
-        hm.put("india",200);
-        hm.put("Usa",20);
-        hm.put("india123",200213);
-        hm.put("in312412dia",2004132421);
+        // hm.put("india",200);
+        // hm.put("Usa",20);
+        // hm.put("india123",200213);
+        // hm.put("in312412dia",2004132421);
     
-        System.out.println(hm.keySet());
-        hm.remove("india");
-        System.out.println(hm.keySet());
+        // System.out.println(hm.keySet());
+        // hm.remove("india");
+        // System.out.println(hm.keySet());
 
-        System.out.println(hm.get("Usa"));
-        System.out.println(hm.get("USA1"));
+        // System.out.println(hm.get("Usa"));
+        // System.out.println(hm.get("USA1"));
 
-        System.out.println(hm.containsKey("Usa"));
-        System.out.println(hm.containsKey("USA1"));
+        // System.out.println(hm.containsKey("Usa"));
+        // System.out.println(hm.containsKey("USA1"));
+
+        Heap pq = new Heap();
+
+        pq.add(30);
+        System.out.println(pq.data+"->"+pq.min());
+        pq.add(10);
+        System.out.println(pq.data+"->"+pq.min());
+        pq.add(6);
+        System.out.println(pq.data+"->"+pq.min());
+        pq.add(100);
+        System.out.println(pq.data+"->"+pq.min());
+        pq.add(3);
+        System.out.println(pq.data+"->"+pq.min());
+        pq.remove();
+        System.out.println(pq.data+"->"+pq.min());
     }
 }
